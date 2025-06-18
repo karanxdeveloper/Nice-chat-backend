d9a53a2938e646dccd146b44877954417d88abae
karan <karan372003@gmail.com>
2025-06-19 02:07:52 +0530

    socket io for real time chat implemented, message controllers done, message routes done

--- server.js
+++ server.js
@@ -6,12 +6,43 @@ import cors from "cors";
 import http from  "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 
 //create Express app and HTTP server
 
 const app = express()
 const server = http.createServer(app)
+
+// Initialize socket.io server
+export const io = new Server(server,{
+    cors:{origin:"*"}
+})
+
+// Store online users
+
+export const userSocketMap = {}; //{userId:socketId}
+
+// Socket.io connection handler
+
+io.on("connection", (socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("user connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+        
+    })
+    
+})
 
 //middleware setup
 app.use(express.json({limit:"4mb"}))
@@ -20,6 +51,7 @@ app.use(cors())
 //Route setup 
 app.use("/api/status",(req,res)=> res.send("server is live"));
 app.use("/api/auth", userRouter)
+app.use("/api/messages",messageRouter)
 
 //Connect to MongoDB
 await connectDB()
