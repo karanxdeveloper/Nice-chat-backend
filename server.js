faeb93f04338e6f1f3715ee9150d3bb3ed7b1eba
karan <karan372003@gmail.com>
2025-07-01 19:26:22 +0530

    fixed cors bug

--- server.js
+++ server.js
@@ -47,7 +47,10 @@ io.on("connection", (socket) => {
 //middleware setup
 // app.use(express.json())
 app.use(express.json({ limit: "10mb" }))
-app.use(cors())
+app.use(cors({
+    origin:"https://nice-chat-gamma.vercel.app",
+    credentials:true
+}))
 
 //Route setup 
 app.use("/api/status", (req, res) => res.send("server is live"));
